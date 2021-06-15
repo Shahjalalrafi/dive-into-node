32669f6b0d7ce55f00c79dbd893b82685c91b8cd
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-06-15 17:34:11 +0600

    file module and http module combining implementation

---
+++ getFile.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./index.html', (err, data) => {
+        if(err) {
+            console.log(err)
+        }else {
+            res.write(data)
+            res.end()
+        }
+    })
+})
+
+
+server.listen(3000, () => console.log('listenning from from 3000'))
